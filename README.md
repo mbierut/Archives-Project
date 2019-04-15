cc699894cffbb82eebc281f5a6880e8dfca16de0
Mikolaj Nalecz <mikolaj.nalecz@hotmail.com>
2019-04-15 02:03:28 +0200

    Refactored for Spring Boot. Removed redundant config etc.

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Spring-Boot-Project
 
 Version of my archives project utilising Spring Boot. Still in early development.
+
+# How to run
+
+You need to have MySQL v5.0+ running on port 3306 with database named "test", username "test" and password "test".
+
+Example docker command:
+
+``` 
+docker run -p 3306:3306 --name mysql-docker-container -e MYSQL_ROOT_PASSWORD=test -e MYSQL_DATABASE=test -e MYSQL_USER=test -e MYSQL_PASSWORD=test mysql:latest
---
+++ src/main/java/pl/mbierut/ArchivesprojectsbApplication.java
@@ -0,0 +1,15 @@
+package pl.mbierut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class ArchivesprojectsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchivesprojectsbApplication.class, args);
+    }
+
+}
